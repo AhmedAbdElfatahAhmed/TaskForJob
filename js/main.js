1a00802d544393af84a2b719963be779ee74b5e3
Ahmed Abd Elfatah <chem.Ahmedaf@gmail.com>
2021-06-14 18:48:53 +0200

    design Footer

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// initialize the Green Audio Player
+new GreenAudioPlayer(".listen-audio");
+// methods for initializing several Green Audio Players:
+GreenAudioPlayer.init({
+  selector: ".player",
+  stopOthersOnPlay: true,
+});
