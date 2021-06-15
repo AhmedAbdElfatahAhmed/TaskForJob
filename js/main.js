43fbc75bdf80115d4bf7f8c7702ad178297e857e
Ahmed Abd Elfatah <chem.Ahmedaf@gmail.com>
2021-06-15 02:43:40 +0200

    add preloader

--- js/main.js
+++ js/main.js
@@ -1,7 +1,103 @@
+
 // initialize the Green Audio Player
-new GreenAudioPlayer(".listen-audio");
+new GreenAudioPlayer(".toAnswer-audio");
 // methods for initializing several Green Audio Players:
 GreenAudioPlayer.init({
   selector: ".player",
   stopOthersOnPlay: true,
 });
+
+// Start play listen and number audio
+const audioIcon = document.querySelector(".caption #audio-icon");
+audioIcon.onclick = () => {
+  document.querySelector("audio#listenAndNumber").play();
+};
+// End play listen and number audio
+
+// declar a five numbers
+let numbers = [1, 2, 3, 4, 5];
+let itemsContent = document.querySelectorAll(".border-container .content");
+let clickedSpan = document.querySelectorAll(".border-container .content span");
+
+// let ul = document.createElement("ul");
+
+clickedSpan.forEach((item) => {
+let ul = document.createElement("ul");
+  item.appendChild(ul);
+  for (let i = 0; i < numbers.length; i++) {
+    let textli=document.createTextNode(numbers[i])
+    let li = document.createElement("li");
+    ul.appendChild(li);
+    li.append(textli)
+    ul.className="ul-style"
+    li.className="li-style"
+    li.classList.add("selected")
+  }
+
+  
+item.onclick=(e)=>
+{   
+  ul.classList.toggle("ul-style");
+  const listItems = ul.children;
+  const listArray = Array.from(listItems);
+  listArray.forEach(list=>
+    {
+      list.onclick=(e)=>
+      {
+   console.log(e.target.innerHTML)
+      }
+    })
+    
+}
+
+
+});
+
+
+
+/** loading page */
+
+
+$("document").ready(function(){
+
+  $(".loader-cover").fadeOut(1000)
+  
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Start preloader fadeOutEffect function
+function fadeOutEffect() {
+  var fadeTarget = document.querySelector(".loader-cover");
+  var fadeEffect = setInterval(function () {
+      if (!fadeTarget.style.opacity) {
+          fadeTarget.style.opacity = 1;
+      }
+      if (fadeTarget.style.opacity > 0) {
+          fadeTarget.style.opacity -= 0.1;
+      } else {
+          clearInterval(fadeEffect);
+      }
+  }, 100);
+}
+
+// End preloader fadeOutEffect function
+
+
+
+// fadeOutEffect() ;
